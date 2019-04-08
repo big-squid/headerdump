cf2514754713f7263c6113cc92caf77f3046e84c
Murray Resinski <murray.resinski@octanner.com>
2019-04-08 09:56:50 -0600

    initial commit

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+                fmt.Printf("value of environment variable %v: %v\n", "ECHO_VAR", os.Getenv("ECHO_VAR"))
+		r.Write(w)
+	})
+	http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+}
+
+
